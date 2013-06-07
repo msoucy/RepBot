bb6340b35da71b3836f3371e717b2a3489d323d3
Russell Harmon <russ@eatnumber1.com>
2013-06-07 13:29:16 -0700

    Support PDP-8 and x86 style reputation.

--- RepBot.py
+++ RepBot.py
@@ -9,24 +9,163 @@ from twisted.internet import protocol, reactor, ssl
 
 from repsys import ReputationSystem
 import admin
+import random
 
+class RepChangeCommand(object):
+    def __init__(self):
+        self.valid = False
+        self.user = None
 
-def is_rep_change(cmd):
-    return cmd.startswith(('++', '--')) or cmd.endswith(('++', '--'))
+    def isValid(self):
+        return self.valid
 
+    def getUser(self):
+        return self.user
 
-def parse_rep_change(cng):
-    name, op = "", ""
-    if cng.startswith(("++", "--")) and cng.endswith(("++", "--")):
-        # Should be an error
-        pass
-    elif cng.startswith(("++", "--")):
-        op = cng[:2]
-        name = cng[2:]
-    elif cng.endswith(("++", "--")):
-        op = cng[-2:]
-        name = cng[:-2]
-    return name, op
+    def setValid(self, valid):
+        self.valid = valid
+
+    def setUser(self, user):
+        self.user = user.split("|")[0].lower()
+
+    def perform(self, val):
+        return NotImplemented
+
+class X86RepChange(RepChangeCommand):
+    def __init__(self, msg):
+        super(X86RepChange, self).__init__()
+
+        m = msg.lower().split()
+        self.atomic = False
+
+        if len(m) < 2:
+            return
+        if m[0] == "lock":
+            self.atomic = True
+            m = m[1:]
+            if len(m) < 2:
+                return
+        suffix = self.getSuffix().lower()
+        if m[0] == "inc" + suffix:
+            self.op = "inc"
+        elif m[0] == "dec" + suffix:
+            self.op = "dec"
+        else:
+            return
+
+        self.setUser(m[1])
+        self.setValid(True)
+
+    def getBits(self):
+        return NotImplemented
+
+    def getSuffix(self):
+        return NotImplemented
+
+    def perform(self, val):
+        if not self.atomic and random.randint(1, 100) <= 10:
+            return val
+        bits = self.getBits() - 1
+        maxval = (2**bits)-1
+        minval = -1 * (2**bits)
+        if self.op == "inc":
+            if val >= maxval:
+                return minval
+            else:
+                return val + 1
+        elif self.op == "dec":
+            if val <= minval:
+                return maxval
+            else:
+                return val - 1
+
+class X86_64RepChange(X86RepChange):
+    def getSuffix(self):
+        return "q"
+
+    def getBits(self):
+        return 64
+
+class X86_32RepChange(X86RepChange):
+    def getSuffix(self):
+        return "l"
+
+    def getBits(self):
+        return 32
+
+class X86_16RepChange(X86RepChange):
+    def getSuffix(self):
+        return "w"
+
+    def getBits(self):
+        return 16
+
+class X86_8RepChange(X86RepChange):
+    def getSuffix(self):
+        return "b"
+
+    def getBits(self):
+        return 8
+
+class PrePostfixRepChange(RepChangeCommand):
+    def __init__(self, msg):
+        super(PrePostfixRepChange, self).__init__()
+
+        m = msg.lower().split()
+
+        starts = msg.startswith(('++', '--'))
+        ends = msg.endswith(('++', '--'))
+
+        if (starts and ends) or (not starts and not ends):
+            return
+
+        if starts:
+            self.setUser(msg[2:])
+            self.op = msg[:2]
+
+        if ends:
+            self.setUser(msg[:-2])
+            self.op = msg[-2:]
+
+        self.setValid(True)
+
+    def perform(self, val):
+        if self.op == "++":
+            return val + 1
+        elif self.op == "--":
+            return val - 1
+
+class PDP8RepChange(RepChangeCommand):
+    def __init__(self, msg):
+        super(PDP8RepChange, self).__init__()
+
+        m = msg.lower().split()
+
+        if len(m) != 2 or m[1] != "iac" or m[0][-1] != ":":
+            return
+
+        self.setUser(m[0][:-1])
+        self.setValid(True)
+
+    def perform(self, val):
+        return val + 1
+
+class RepChangeCommandFactory(object):
+    REP_CHANGERS = [
+        PrePostfixRepChange,
+        X86_64RepChange,
+        X86_32RepChange,
+        X86_16RepChange,
+        X86_8RepChange,
+        PDP8RepChange
+    ]
+
+    def parse(self, msg):
+        for changer in self.REP_CHANGERS:
+            c = changer(msg)
+            if c.isValid():
+                return c
+        return None
 
 
 def getNameFromIdent(name):
@@ -86,9 +225,9 @@ class RepBot(irc.IRCClient):
     def admin(self, user, msg):
         admin.admin(self, user, msg)
 
-    def handleChange(self, user, cmd):
-        name, op = parse_rep_change(cmd)
-        if name.split("|")[0].lower() == user.split("|")[0].lower():
+    def handleChange(self, user, changer):
+        name = changer.getUser()
+        if name == user.split("|")[0].lower():
             self.msg(user, "Cannot change own rep")
             return
         currtime = time.time()
@@ -96,12 +235,9 @@ class RepBot(irc.IRCClient):
                             for val in self.users.get(user, [])
                             if (currtime - val) < self.cfg["timelimit"]]
         if len(self.users[user]) < self.cfg["replimit"]:
-            if op == "++":
-                self.reps.incr(name)
-                self.users[user].append(time.time())
-            elif op == "--":
-                self.reps.decr(name)
-                self.users[user].append(time.time())
+            rep = self.reps.get(name)
+            self.reps.set(name, changer.perform(rep))
+            self.users[user].append(time.time())
         else:
             self.msg(user, "You have reached your rep limit. You can give more rep in {0} seconds"
                      .format(int(self.cfg["timelimit"] - (currtime - self.users[-1]))))
@@ -121,9 +257,10 @@ class RepBot(irc.IRCClient):
         args = msg.split()
         cmd = args[0] if args else ""
         args = args[1:] if args else []
+        changer = RepChangeCommandFactory().parse(msg)
 
-        if len(args) == 0 and is_rep_change(cmd):
-            self.handleChange(user, cmd)
+        if changer != None:
+            self.handleChange(user, changer)
         elif cmd in ("rep",):
             self.msg(channel, self.reps.tell(args[0] if args else user))
         elif cmd in ("ver", "version", "about"):
@@ -155,7 +292,7 @@ class RepBot(irc.IRCClient):
                 msg = msg[len(self.cfg["nick"]) + 1:].strip()
             elif msg.startswith('!'):
                 msg = msg[1:]
-            elif not is_rep_change(msg):
+            elif RepChangeCommandFactory().parse(msg) == None:
                 return
         elif msg.startswith('!'):
             msg = msg[1:]
